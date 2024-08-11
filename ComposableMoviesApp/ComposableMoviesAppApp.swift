604425504daaadd483b7a5e3b77cc79b1c26a474
Andersson <andersson@Anderssons-MacBook-Pro.local>
2024-08-11 13:36:14 -0500

    First commit

---
+++ ComposableMoviesApp/ComposableMoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableMoviesAppApp.swift
+//  ComposableMoviesApp
+//
+//  Created by Andersson on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ComposableMoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
